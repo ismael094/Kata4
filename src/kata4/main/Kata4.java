7552ac5273115b1bf4e9fe6b948aa8d05ec0464a
Usuario <Usuario@10.22.148.83>
2018-10-25 11:24:55 +0100

    Kata4 versión 2

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package kata4.main;
 
 import java.util.List;
@@ -12,22 +8,44 @@ import kata4.view.HistogramDisplay;
 import static kata4.view.MailHistogramBuilder.build;
 import static kata4.view.MailListReader.read;
 
-/**
- *
- * @author Usuario
- */
-public class Kata4 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws Exception {
-        
-        
-        List<Mail> a = read("C:\\Users\\Usuario\\Documents\\NetBeansProjects\\Kata4\\src\\kata4\\main\\email.txt");
-        HistogramDisplay histo = new HistogramDisplay(build(a));
-        
-        histo.execute();
+public class Kata4 {
+    
+    private final String fileName;
+    private List<Mail> mailList;
+    private Histogram<String> histogram;
+    private HistogramDisplay histoDisplay;
+            
+    public static void main(String[] args)  {
+        Kata4 k = new Kata4("C:\\Users\\Usuario\\Documents\\NetBeansProjects\\Kata4\\src\\kata4\\main\\email.txt");
+        k.execute();
+    }
+
+    public Kata4(String fileName) {
+        this.fileName = fileName;
+    }
+    
+    public void execute() {
+        input();
+        process();
+        output();
+    }
+    
+    public void input() {
+        try {
+           mailList = read(fileName); 
+        } catch (Exception e) {
+            System.out.println("Ha ocurrido un error con el fichero");
+        }
+    }
+    
+    public void process() {
+        histogram = build(mailList);
+    }
+    
+    public void output() {
+        histoDisplay = new HistogramDisplay(histogram);
+        histoDisplay.execute();
     }
     
 }
